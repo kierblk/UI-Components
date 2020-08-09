3007aae71a02c731cd01f78cdea04be27f049148
saramccombs <theSaraMcCombs@gmail.com>
2020-08-08 20:07:12 -0400

    refac(App.js): Remove create-react-app boilerplate code.
    Following Material UI docs, build out rich object folder tree with hard coded data.
    
    Will remove hard coded data at a later point once desired UI is achieved.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Hard coded data for the time being
+// Goal to fetch from JSON file once basic structure is achieved
+
+const data = {
+  "type": "folder",
+  "name": "root",
+  "modified": "2020-08-08T00:00:00.000Z",
+  "size": 100,
+  "children": [
+  {
+      "type": "folder",
+      "name": "documents",
+      "modified": "2020-08-07T00:00:00.000Z",
+      "size": 10,
+      "children": [
+        {
+          "type": "file",
+          "name": "document.txt",
+          "modified": "2020-08-06T00:00:00.000Z",
+          "size": 80,
+          "children": []
+      }
+      ]
+  },
+  {
+      "type": "file",
+      "name": "home.html",
+      "modified": "2020-08-06T00:00:00.000Z",
+      "size": 80,
+      "children": []
+  },
+  {
+      "type": "file",
+      "name": "about.html",
+      "modified": "2020-08-05T00:00:00.000Z",
+      "size": 10,
+      "children": []
+  }
+  ]
 }
 
-export default App;
+const useStyles = makeStyles({
+  root: {
+    height: 110,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+});
+
+export default function RecursiveTreeView() {
+  const classes = useStyles();
+
+  const renderFolderTree = (folders) => (
+    <TreeItem key={folders.name} nodeId={folders.name} label={folders.name}>
+      {Array.isArray(folders.children) ? folders.children.map((node) => renderFolderTree(node)) : null}
+    </TreeItem>
+  );
+
+  return (
+    <>
+      <div className="left">
+        <TreeView
+        className={classes.root}
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpanded={['root']}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        {renderFolderTree(data)}
+      </TreeView>
+      </div>
+      <div className="right">
+        {/* Folder contents will render here */}
+      </div>
+    </>
+  );
+}
